46bb7f43d6a67ed96dbf8928a775275199960d34
d.rezyaev <d.rezyaev@tbank.ru>
2026-08-10 18:36:25 +0300

    filter by the international league id

--- api/matches.ts
+++ api/matches.ts
@@ -4,9 +4,12 @@ export default {
 	async fetch(request: Request) {
 		const status = new URL(request.url).searchParams.get("status");
 
-		const response = await fetch(`https://api.pandascore.co/dota2/matches/${status}?sort=begin_at&page=1&per_page=10`, {
-			headers: { accept: "application/json", Authorization: `Bearer ${process.env.PANDASCORE_TOKEN}` },
-		});
+		const response = await fetch(
+			`https://api.pandascore.co/dota2/matches/${status}?filter[league_id]=4106&sort=begin_at&page=1&per_page=100`,
+			{
+				headers: { accept: "application/json", Authorization: `Bearer ${process.env.PANDASCORE_TOKEN}` },
+			},
+		);
 
 		const data = await response.json();
 
