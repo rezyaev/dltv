565a021122c3f546cead92915bedd2b0185a8402
d.rezyaev <d.rezyaev@tbank.ru>
2026-08-12 16:48:39 +0300

    refactor tabs into App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,32 @@
-<script>
+<script lang="ts">
+	import { fetchMatches } from "./views/matches/matches";
 	import Matches from "./views/matches/Matches.svelte";
+
+	let tab = $state<"upcoming" | "past" | "group">("upcoming");
 </script>
 
 <main class="w-full h-full bg-base-100 p-4">
-	<Matches />
+	<div class="tabs tabs-box mb-4">
+		<input type="radio" name="my_tabs_1" class="tab flex-1" aria-label="Матчи" bind:group={tab} value="upcoming" />
+		<input type="radio" name="my_tabs_1" class="tab flex-1" aria-label="Результаты" bind:group={tab} value="past" />
+		<input type="radio" name="my_tabs_1" class="tab flex-1" aria-label="Группа" bind:group={tab} value="group" />
+	</div>
+
+	{#if tab === "upcoming"}
+		{#await fetchMatches("running") then matches}
+			{#if matches.length > 0}
+				<Matches {matches} live={true} showResults={true} />
+			{/if}
+		{/await}
+
+		{#await fetchMatches("upcoming") then matches}
+			<Matches {matches} />
+		{/await}
+	{:else if tab === "past"}
+		{#await fetchMatches("past") then matches}
+			<Matches {matches} showResults={true} />
+		{/await}
+	{:else if tab === "group"}
+		group
+	{/if}
 </main>
